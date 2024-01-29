978414939d5b2e4abcf17a84863c47485a375a80
dominikn <dominik.nwk@gmail.com>
2024-01-29 18:58:30 +0100

    working on new setup with justfile

---
+++ ros_entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+output=$(husarnet-dds singleshot) || true
+if [[ "$HUSARNET_DDS_DEBUG" == "TRUE" ]]; then
+  echo "$output"
+fi
+
+# setup ros environment
+source "/opt/ros/$ROS_DISTRO/setup.bash"
+test -f "/ros2_ws/install/setup.bash" && source "/ros2_ws/install/setup.bash"
+
+if [ -z "$USER" ]; then
+    export USER=root
+elif ! id "$USER" &>/dev/null; then
+    useradd -ms /bin/bash "$USER"
+fi
+
+# <additional-user-commands>
+
+if [ $# -eq 0 ]; then
+    exec gosu $USER /bin/bash
+else
+    exec gosu $USER "$@"
+fi
