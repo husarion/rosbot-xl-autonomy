147eb7989d0466c90ba39d616afdc483efb19c6b
dominikn <dominik.nwk@gmail.com>
2024-02-01 18:34:05 +0100

    work in progress

--- ros_entrypoint.sh
+++ ros_entrypoint.sh
@@ -8,7 +8,6 @@ fi
 
 # setup ros environment
 source "/opt/ros/$ROS_DISTRO/setup.bash"
-test -f "/ros2_ws/install/setup.bash" && source "/ros2_ws/install/setup.bash"
 
 if [ -z "$USER" ]; then
     export USER=root
@@ -16,7 +15,8 @@ elif ! id "$USER" &>/dev/null; then
     useradd -ms /bin/bash "$USER"
 fi
 
-# <additional-user-commands>
+test -f "/ros2_ws/install/setup.bash" && source "/ros2_ws/install/setup.bash"
+gosu $USER bash -c 'ros2 run healthcheck_pkg healthcheck_node &'
 
 if [ $# -eq 0 ]; then
     exec gosu $USER /bin/bash
